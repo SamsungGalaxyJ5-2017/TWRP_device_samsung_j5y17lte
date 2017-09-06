4d18a143dedc22938a6343c5e44e5b494d581541
ananjaser1211 <ananjaser@gmail.com>
2017-09-06 15:20:54 +0400

    Added TWRP Flags.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -48,3 +48,48 @@ TARGET_PREBUILT_DTB := device/samsung/j5y17lte/dt.img
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 BOARD_CUSTOM_BOOTIMG_MK :=  device/samsung/j5y17lte/bootimg.mk
+
+
+# TWRP specific build flags
+TW_THEME := portrait_hdpi
+TW_HAS_MTP := true
+RECOVERY_SDCARD_ON_DATA := true
+BOARD_HAS_NO_REAL_SDCARD := true
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/13600000.usb/13600000.dwc3/gadget/lun%d/file"
+TW_BRIGHTNESS_PATH := "/sys/devices/14800000.dsim/backlight/panel/brightness"
+TW_MAX_BRIGHTNESS := 255
+TW_DEFAULT_BRIGHTNESS := 162
+TW_NO_REBOOT_BOOTLOADER := true
+TW_HAS_DOWNLOAD_MODE := true
+TW_MTP_DEVICE := "/dev/mtp_usb"
+TW_EXCLUDE_SUPERSU := true
+TW_INCLUDE_NTFS_3G := true
+TW_EXTRA_LANGUAGES := true
+TW_USE_NEW_MINADBD := true
+TWRP_INCLUDE_LOGCAT := true
+
+# Color fix
+TARGET_RECOVERY_PIXEL_FORMAT := "ABGR_8888"
+
+# Encryption support
+TW_INCLUDE_CRYPTO := true
+#TW_INCLUDE_CRYPTO_SAMSUNG := true
+#TARGET_HW_DISK_ENCRYPTION := true
+
+# Debug flags
+TWRP_INCLUDE_LOGCAT := true
+TARGET_USES_LOGD := true
+TWRP_EVENT_LOGGING := true
+
+# Init properties from bootloader version, ex. model info
+#TARGET_UNIFIED_DEVICE := true
+#TARGET_INIT_VENDOR_LIB := libinit_sec
+#TARGET_RECOVERY_DEVICE_MODULES := libinit_sec
+#TARGET_LIBINIT_DEFINES_FILE := device/samsung/j5y17lte/init/init_sec.cpp
+
+# Charger/Healthd
+BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/power_supply/battery/batt_lp_charging
+BOARD_CHARGER_ENABLE_SUSPEND := true
+BOARD_CHARGER_SHOW_PERCENTAGE := true
+BACKLIGHT_PATH := "/sys/class/backlight/panel/brightness"
+CHARGING_ENABLED_PATH := /sys/class/power_supply/battery/batt_lp_charging
